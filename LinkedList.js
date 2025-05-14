a4f82991f7e1d3e2f4975bbfe47effca358da439
williamandrews2 <121400607+williamandrews2@users.noreply.github.com>
2025-05-13 20:46:16 -0500

    Initial commit.

---
+++ LinkedList.js
@@ -0,0 +1,77 @@
+class Node {
+  constructor(element) {
+    this.element = element;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.length = 0;
+  }
+
+  append(element) {
+    let node = new Node(element);
+    if (this.head === null) {
+      this.head = node; // set the head in the case of an empty linked list
+    } else {
+      let currentNode = this.head;
+      while (currentNode.next) {
+        currentNode = currentNode.next;
+      }
+      currentNode.next = node;
+    }
+
+    this.length++;
+  }
+
+  prepend(element) {
+    let node = new Node(element);
+
+    if (this.head === null) {
+      this.head = node;
+    }
+
+    node.next = this.head;
+    this.head = node;
+    this.length++;
+  }
+
+  isEmpty() {
+    return this.length === 0;
+  }
+
+  size() {
+    return this.length;
+  }
+
+  head() {}
+  tail() {}
+  at(index) {}
+  pop() {}
+  contains(value) {}
+  find(element) {}
+  delete(element) {}
+  insertAt(value, index) {}
+  removeAt(value, index) {}
+
+  printToArray() {
+    let result = [];
+    let currentNode = this.head;
+    while (currentNode) {
+      result.push(currentNode.element);
+      currentNode = currentNode.next;
+    }
+    console.log(result);
+  }
+}
+
+const list = new LinkedList();
+list.append(10);
+list.append(20);
+list.append(30);
+list.append(40);
+
+console.log(list.size());
+list.printToArray();
